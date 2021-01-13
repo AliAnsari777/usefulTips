a49c78d3b64f24e53066689b7460fc3e21d4929b
ALi Ansari <alidk2013@gmail.com>
2021-01-12 22:57:32 -0700

    initial commit for usefull tips repository

---
+++ src/main/java/practice/usefultips/UsefultipsApplication.java
@@ -0,0 +1,13 @@
+package practice.usefultips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefultipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsefultipsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/practice/usefultips/UsefultipsApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.usefultips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefultipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
