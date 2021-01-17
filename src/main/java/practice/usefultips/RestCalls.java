dbb617a89aa5f87c3d517de3df16a6336d7cd465
ALi Ansari <alidk2013@gmail.com>
2021-01-16 23:58:16 -0700

    new method

--- src/main/java/practice/usefultips/RestCalls.java
+++ src/main/java/practice/usefultips/RestCalls.java
@@ -1,4 +1,10 @@
 package practice.usefultips;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class RestCalls {
+    HttpEntity httpEntity;
+    RestTemplate restTemplate = new RestTemplate();
+
 }
